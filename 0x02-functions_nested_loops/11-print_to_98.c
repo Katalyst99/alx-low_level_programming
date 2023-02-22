01b95f156b6dc5ce3d8e5de074125290bce3b323
Katalyst99 <katleho333@gmail.com>
2023-02-22 06:58:31 -0800

    Nine eight

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,25 @@
  */
 void print_to_98(int n)
 {
-	if ( n > 98)
+	int i, c;
+
+	if (n <= 98)
 	{
-		for (; n > 98; n--)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", n);
+			if (i != 98)
+				printf("%i, ", i);
+			else if (i == 98)
+				printf("%i\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (c = n; c >= 98; c--)
+		{
+			if (c != 98)
+				printf("%i, ", c);
+			else if (c == 98)
+				printf("%i\n", c);
 		}
 	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d`\n", n);
 }
