b12e767e52119fc23b8002104f9c6508f7264919
Katalyst99 <katleho333@gmail.com>
2023-02-23 03:16:18 -0800

    C is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two integers.
+ * @a: first varialbe
+ * @b: second variable
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
