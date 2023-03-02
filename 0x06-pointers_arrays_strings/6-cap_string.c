b1a80b67cc921fcb52bc4f91cdb93d88e4897aea
Katalyst99 <katleho333@gmail.com>
2023-03-02 04:58:29 -0800

    Fixing the cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,17 @@ char *cap_string(char *str)
 	int i = 0, c;
 	int ws[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(str + i) >= 'a' && *(str + i) <= 'z')
-		*(str + i) = *(str + i) - ' ';
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - ' ';
 	i++;
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 	{
 		for (c = 0; c < 13; c++)
 		{
-			if (*(str + i) == ws[c])
+			if (str[i] == ws[c])
 			{
-				if ((*(str + (i + 1)) >= 'a') && (*(str + (i + 1)) <= 'z'))
-					*(str + (i + 1)) = *(str + (i + 1)) - ' ';
+				if ((str[i + 1] >= 'a') && (str[i + 1] <= 'z'))
+					str[i + 1] = str[i + 1] - ' ';
 			}
 		}
 		i++;
