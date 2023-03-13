d98be529c523ec2e7c083aed5d3f5ae1788f1da2
Katalyst99 <katleho333@gmail.com>
2023-03-13 11:38:27 -0700

    Concat strings along

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: The first string
+ * @s2: The second string
+ * Return: Pointer pointing to a newly allocated space in memory which,
+ * contains the concatenated strings.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *catstr;
+
+	unsigned int i, j, k;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (i = 0; s1[i] || s2[i]; i++)
+	{
+		j = 0;
+		j++;
+	}
+
+	catstr = malloc(sizeof(char) * j);
+
+	if (catstr == NULL)
+	{
+		return (NULL);
+	}
+	k = 0;
+	for (i = 0; s1[i]; i++)
+	{
+		catstr[k++] = s1[i];
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		catstr[k++] = s2[i];
+	}
+	return (catstr);
+}
