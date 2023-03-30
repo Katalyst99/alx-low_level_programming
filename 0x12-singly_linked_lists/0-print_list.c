196ed2e107c92b7ce8b7375d0575786b0b6a0d81
Katalyst99 <katleho333@gmail.com>
2023-03-30 14:50:20 -0700

    Fixing errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
-		node++;
+		nodes++;
 	}
-	return (node);
+	return (nodes);
 }
