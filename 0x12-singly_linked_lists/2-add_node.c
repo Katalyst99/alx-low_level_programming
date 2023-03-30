d8fb740bfe27c637a6ea495af24e3a4c714951d6
Katalyst99 <katleho333@gmail.com>
2023-03-30 16:26:37 -0700

    Function error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,27 +10,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	char *sd;
-	int len;
+	size_t len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	sd = strdup(str);
-	if (sd == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+	new->str = strdup(str);
 
-	for (len = 0; str[len];)
-		len++;
+	for (len = 0; str[len]; len++)
+		;
 
-	new->str = sd;
 	new->len = len;
 	new->next = *head;
 	*head = new;
 
-	return (new);
+	return (*head);
 }
