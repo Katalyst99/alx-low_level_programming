f13850aebf3994e6f084ca5ae3ee2045183a8dbe
Katalyst99 <katleho333@gmail.com>
2023-06-15 16:49:05 -0700

    Realloc and range

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: The pointer to the memory previously allocated
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: ptr, If new_size == old_size
+ * NULL, If new_size is equal to zero, and ptr is not NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range -  creates an array of integers.
+ * @min: The minimum value.
+ * @max: The maximum value.
+ * Return: pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int *a;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	a = malloc(sizeof(int) * ((max - min) + 1));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min <= max; i++, min++)
+	{
+		a[i] = min;
+	}
+	return (a);
+}
