30d8d0d9ee88614a16b7ae0c080190b4a334aeb8
Katalyst99 <katleho333@gmail.com>
2023-06-15 14:11:16 -0700

    Code fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len]; len2++)
 		;
 
-	if (n >= len2)
+	if (n > len2)
 		cstr = malloc(sizeof(char) * (len + len2 + 1));
 	else
 		cstr = malloc(sizeof(char) * (len + n + 1));
