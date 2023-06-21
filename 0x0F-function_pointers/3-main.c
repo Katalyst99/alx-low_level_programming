400413f7678ffa41869838f3883171ea96c6745c
Katalyst99 <katleho333@gmail.com>
2023-06-21 16:18:48 -0700

    Fixed errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 	int (*opf)(int, int);
+	char op;
 
 	if (argc != 4)
 	{
@@ -21,11 +22,18 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	opf = get_op_func(argv[2]);
+	op = *argv[2];
 
 	if (opf == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+
+	if ((op == '/' || op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	else
 	{
