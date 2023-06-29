f97cfc8e158ac6bda3d1ff1420901d5862a0454c
Katalyst99 <katleho333@gmail.com>
2023-06-29 12:46:12 -0700

    Code fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != 0)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
