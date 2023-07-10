c1db19d19def019b2278cf5f0a33181f0e5c6039
Katalyst99 <katleho333@gmail.com>
2023-07-10 15:13:55 -0700

    Error fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,14 +21,17 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	while (text_content[len])
+	if (text_content != NULL)
 	{
-		len++;
-	}
-	wr = write(fd, text_content, len);
-	if (wr == -1)
-	{
-		return (-1);
+		while (text_content[len])
+		{
+			len++;
+		}
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
