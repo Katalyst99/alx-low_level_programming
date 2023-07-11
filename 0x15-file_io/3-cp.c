138ee4a2ac5bad978f727702cd67145bb81e351d
Katalyst99 <katleho333@gmail.com>
2023-07-10 18:06:15 -0700

    Code fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,13 +23,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(98);
 	}
-	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fdt = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdt == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((r = read(fdf, buffer, sizeof(buffer))) > 0)
 	{
 		wr = write(fdt, buffer, r);
-		if (wr == -1)
+		if (wr != r)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (r == -1)
