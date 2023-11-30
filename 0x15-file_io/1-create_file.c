e5437ffad7b7d89e845fce14f9c63beed141b579
Katalyst99 <katleho333@gmail.com>
2023-11-30 10:00:33 -0800

    Hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len])
+		for (len = 0; text_content[len]; len++)
 		{
-			len++;
+			;
 		}
 	}
 	else
