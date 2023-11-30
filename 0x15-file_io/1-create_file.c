0e0687ce3e4b19d2c00a7e4d219b16e11d7e7047
Katalyst99 <katleho333@gmail.com>
2023-11-30 09:49:40 -0800

    Fixing error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	o = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (o == -1)
 	{
 		return (-1);
