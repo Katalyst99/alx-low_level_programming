c338af78bf20da35dcef9d069ff822e029f7b486
Katalyst99 <katleho333@gmail.com>
2024-01-11 13:59:44 -0800

    Code fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	}
 
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (tmp == NULL)
@@ -26,16 +27,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
 	new_n = malloc(sizeof(dlistint_t));
 	if (new_n == NULL)
 	{
 		return (NULL);
 	}
+	else
+	{
 		new_n->n = n;
 		new_n->next = tmp->next;
 		new_n->prev = tmp;
 		tmp->next->prev = new_n;
 		tmp->next = new_n;
-
+	}
 		return (new_n);
 }
