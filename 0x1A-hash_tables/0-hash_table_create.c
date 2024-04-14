3bb6037e99379284dcea2dfa27209b72979d8889
Katalyst99 <katleho333@gmail.com>
2024-04-14 05:28:37 -0700

    Roslving error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	htable->size = size;
 	htable->array = malloc(sizeof(hash_node_t *) * size);
-	if (htable->array)
+	if (htable->array == NULL)
 	{
 		return (NULL);
 	}
