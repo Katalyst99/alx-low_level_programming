ad706370164ecc07ba1bd5586203e3d48c708488
Katalyst99 <katleho333@gmail.com>
2024-04-17 08:43:21 -0700

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print -  prints a hash table.
+ * @ht: The hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *hnode;
+	int flag = 0;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		hnode = ht->array[i];
+		while (hnode)
+		{
+			if (flag != 0)
+			{
+				printf(", ");
+			}
+			else
+				printf("'%s': '%s'", hnode->key, hnode->value);
+			hnode = hnode->next;
+		}
+	}
+	putchar('}');
+	putchar('\n');
+}
